1280d92cfcec7beba13d0a2e10d9a4d462d8cf6e
pitang1965 <pitang1965@gmail.com>
2021-12-18 20:56:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-urbpor
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-urbpor)
---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div class='content-center'>
+    <h1>Over 40 Web Club</h1>
+    <a href="https://over40web.club/" target="_blank">こちらに移動しました。</a>
+  </div>
+`;
