cc1188475af148066c46221bedd55de5776b2846
pitang1965 <pitang1965@gmail.com>
2021-12-18 21:58:52 +0900

    1 file modified, 1 deleted

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import './style.css';
 document.querySelector('#app').innerHTML = `
   <div class='content-center'>
     <h1>Over 40 Web Club</h1>
-    <a href="https://over40web.club/" target="_blank">こちらに移動しました。</a>
+    <a href="https://over40web.club/" target="_self">こちらに移動しました。</a>
   </div>
 `;
